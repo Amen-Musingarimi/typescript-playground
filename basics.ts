57473de59a9f70c6d747732a9be24aa2e9ac6ec1
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-10-12 09:45:42 +0200

    Add generics

--- basics.ts
+++ basics.ts
@@ -85,3 +85,19 @@ students = [
     age: 29,
   },
 ];
+
+// Functions and Types
+function add(a: number, b: number) {
+  return a + b;
+}
+
+// Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3, 4];
+
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
